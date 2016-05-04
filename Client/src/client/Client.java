04655c8ee9bdfd327b1ca143e58dcd290ab68405
TagoWill <tagowill@gmail.com>
2016-05-04 17:53:45 +0100

    Voter: Missing threads and timeouts. Voter: voter is working and vote correctly

--- Client/src/client/Client.java
+++ Client/src/client/Client.java
@@ -5,6 +5,7 @@ package client;
  */
 public class Client {
     public static void main(String[] args){
-
+        Voter voter = new Voter();
+        voter.TestVoter();
     }
 }
--- Client/src/client/Voter.java
+++ Client/src/client/Voter.java
@@ -8,56 +8,63 @@ package client;
 import service.InsulinDoseCalculator;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Voter {
 
-    public Voter(){
+    Map<Integer, Integer> results;
 
+    public Voter(){
+        results = new HashMap<>();
     }
 
-    public static void TestVoter(){
+    public void TestVoter(){
 
         List<WebServiceHandler> webservices = new ArrayList<>();
 
         webservices.add(new WebServiceHandler("http://qcsproject1-qcsproject.rhcloud.com/InsulinDoseCalculator/?wsdl","InsulinDoseCalculator", "InsulinDoseCalculatorPort", "http://server/"));
         webservices.add(new WebServiceHandler("http://qcsa1-beardsdei.rhcloud.com/qcsa1/InsulinDoseCalculator?wsdl","InsulinDoseCalculatorService", "InsulinDoseCalculatorPort", "http://server/"));
-        webservices.add(new WebServiceHandler("http://insulincalculator-aybareon.rhcloud.com:80/InsulinCalculatorTomcat/InsulinCalculator?wsdl","InsulinCalculator", "InsulinCalculatorPort", "http://insulincalculator-aybareon.rhcloud.com/InsulinCalculatorTomcat"));
+        webservices.add(new WebServiceHandler("http://insulincalculator-aybareon.rhcloud.com:80/InsulinCalculatorTomcat/InsulinCalculator?wsdl","InsulinCalculator", "InsulinCalculatorPort", "http://server/"));
+        webservices.add(new WebServiceHandler("http://webservice-sqdcourse.rhcloud.com/InsulinDoseCalculator?wsdl","InsulinDoseCalculatorService", "InsulinDoseCalculatorPort", "http://server/"));
         InsulinDoseCalculator proxy = null;
 
         for(WebServiceHandler x: webservices){
             try {
                 proxy = x.getProxy();
-                System.out.println("Resultado: " + proxy.mealtimeInsulinDose(60, 12, 200, 100, 25));
+                //System.out.println("Resultado: " + proxy.mealtimeInsulinDose(60, 12, 200, 100, 25));
+                //System.out.println("Resultado: " + proxy.backgroundInsulinDose(79));
+                ArrayList<Integer> lista1 = new ArrayList<>();
+                ArrayList<Integer> lista2 = new ArrayList<>();
+                lista1.add(2);
+                lista1.add(8);
+                lista2.add(32);
+                lista2.add(83);
+                //System.out.println("Resultado: " + proxy.personalSensitivityToInsulin(6, lista1, lista2));
+
+                //int singleresult = proxy.backgroundInsulinDose(79);
+                //int singleresult = proxy.mealtimeInsulinDose(60, 12, 200, 100, 25);
+                int singleresult = proxy.personalSensitivityToInsulin(6, lista1, lista2);
+                System.out.println("Single result: " + singleresult);
+                Integer freq = results.get(singleresult);
+                results.put(singleresult, (freq == null) ? 1 : freq + 1);
+
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
 
-        /*
-        try {
-            proxy = webservice.getProxy();
-            System.out.println("resultado da primeira funcao: " + proxy.backgroundInsulinDose(79));
-        } catch (Exception e) {
-            e.printStackTrace();
+        int max = -1;
+        int mostFrequent = -1;
+
+        for(Map.Entry<Integer, Integer> x: results.entrySet()){
+            //Metade do numero total de webservices testados
+            if (x.getValue() > max && x.getValue() >= (results.size() / 2)) {
+                mostFrequent = x.getKey();
+                max = x.getValue();
+            }
         }
-        */
-
-
-        /*InsulinDoseCalculator_Service service = new InsulinDoseCalculator_Service();
-
-        InsulinDoseCalculator proxy = service.getInsulinDoseCalculatorPort();
-        int bolus_ins_replace_dose = proxy.mealtimeInsulinDose(60, 12, 200, 100, 25);
-        int back_ins = proxy.backgroundInsulinDose(79);
-        ArrayList<Integer> lista1 = new ArrayList<>();
-        ArrayList<Integer> lista2 = new ArrayList<>();
-        lista1.add(2);
-        lista1.add(8);
-        lista2.add(32);
-        lista2.add(83);
-        int personal = proxy.personalSensitivityToInsulin(6, lista1, lista2);
-        System.out.println("resultado da primeira funcao: " + bolus_ins_replace_dose);
-        System.out.println("resultado da segunda funcao: " + back_ins);
-        System.out.println("resultado da terceira funcao: " + personal);*/
+        System.out.println("Resultado do votador: " + mostFrequent);
     }
 }
