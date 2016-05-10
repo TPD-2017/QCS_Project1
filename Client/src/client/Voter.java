a082ce9fafe5e1d8720c41bfcb285b64cf52e5e5
TagoWill <tagowill@gmail.com>
2016-05-10 15:43:58 +0100

    More tests and more webservices.

--- Client/src/client/Voter.java
+++ Client/src/client/Voter.java
@@ -12,49 +12,26 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Voter {
+class Voter {
 
     Map<Integer, Integer> results;
+    List<WebServiceHandler> webservices;
 
-    public Voter(){
+    Voter(){
         results = new HashMap<>();
-    }
 
-    public void TestVoter(){
-
-        List<WebServiceHandler> webservices = new ArrayList<>();
+        webservices = new ArrayList<>();
 
         webservices.add(new WebServiceHandler("http://qcsproject1-qcsproject.rhcloud.com/InsulinDoseCalculator/?wsdl","InsulinDoseCalculator", "InsulinDoseCalculatorPort", "http://server/"));
         webservices.add(new WebServiceHandler("http://qcsa1-beardsdei.rhcloud.com/qcsa1/InsulinDoseCalculator?wsdl","InsulinDoseCalculatorService", "InsulinDoseCalculatorPort", "http://server/"));
         webservices.add(new WebServiceHandler("http://insulincalculator-aybareon.rhcloud.com:80/InsulinCalculatorTomcat/InsulinCalculator?wsdl","InsulinCalculator", "InsulinCalculatorPort", "http://server/"));
         webservices.add(new WebServiceHandler("http://webservice-sqdcourse.rhcloud.com/InsulinDoseCalculator?wsdl","InsulinDoseCalculatorService", "InsulinDoseCalculatorPort", "http://server/"));
-        InsulinDoseCalculator proxy = null;
+        webservices.add(new WebServiceHandler("http://qcs07.dei.uc.pt:8080/InsulinCalculator?wsdl","InsulinCalculatorService", "InsulinCalculatorPort", "http://server/"));
+        webservices.add(new WebServiceHandler("http://sgd46.dei.uc.pt:8088/InsulinDoseCalculator?wsdl","InsulindDoseCalculator", "InsulinDoseCalculatorPort", "http://server/"));
+        webservices.add(new WebServiceHandler("http://qcsa1-ran1234.rhcloud.com/server/InsulinDoseCalculator?wsdl","InsulinDoseCalculatorService", "InsulinDoseCalculatorPort", "http://server/"));
+    }
 
-        for(WebServiceHandler x: webservices){
-            try {
-                proxy = x.getProxy();
-                //System.out.println("Resultado: " + proxy.mealtimeInsulinDose(60, 12, 200, 100, 25));
-                //System.out.println("Resultado: " + proxy.backgroundInsulinDose(79));
-                ArrayList<Integer> lista1 = new ArrayList<>();
-                ArrayList<Integer> lista2 = new ArrayList<>();
-                lista1.add(2);
-                lista1.add(8);
-                lista2.add(32);
-                lista2.add(83);
-                //System.out.println("Resultado: " + proxy.personalSensitivityToInsulin(6, lista1, lista2));
-
-                //int singleresult = proxy.backgroundInsulinDose(79);
-                //int singleresult = proxy.mealtimeInsulinDose(60, 12, 200, 100, 25);
-                int singleresult = proxy.personalSensitivityToInsulin(6, lista1, lista2);
-                System.out.println("Single result: " + singleresult);
-                Integer freq = results.get(singleresult);
-                results.put(singleresult, (freq == null) ? 1 : freq + 1);
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
+    void Majority(){
         int max = -1;
         int mostFrequent = -1;
 
@@ -67,4 +44,40 @@ public class Voter {
         }
         System.out.println("Resultado do votador: " + mostFrequent);
     }
+
+    void TestVoter(){
+
+        InsulinDoseCalculator proxy = null;
+
+        for(WebServiceHandler x: webservices){
+            try {
+                proxy = x.getProxy();
+                ArrayList<Integer> lista1 = new ArrayList<>();
+                ArrayList<Integer> lista2 = new ArrayList<>();
+                lista1.add(1);
+                lista1.add(6);
+                lista1.add(8);
+                lista1.add(9);
+                lista2.add(32);
+                lista2.add(61);
+                lista2.add(91);
+                lista2.add(88);
+                //int singleresult = proxy.backgroundInsulinDose(79);
+                int singleresult = proxy.mealtimeInsulinDose(95, 10, 100, 120, 50);
+                //int singleresult = proxy.personalSensitivityToInsulin(4, lista1, lista2);
+                System.out.println("Single result: " + singleresult);
+                Integer freq = results.get(singleresult);
+                results.put(singleresult, (freq == null) ? 1 : freq + 1);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        Majority();
+    }
+}
+
+class Connection extends Thread{
+
 }
